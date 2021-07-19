8e80f4588ca2ac9546546517cb81c091a5892e0b
Brendon Gama <brendon.scliarcf18@gmail.com>
2021-07-19 18:08:13 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+
+import Home from './components/Home';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    spacing: 4,
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3f51b5',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />{' '}
+    </ThemeProvider>
+  );
+}
+
+export default App;
