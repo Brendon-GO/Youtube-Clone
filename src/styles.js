6a2d1fe6404ff4f1f56a2f2504efbed3d22c1a92
Brendon Gama <brendon.scliarcf18@gmail.com>
2021-07-20 15:38:40 -0300

    Created new files

---
+++ src/styles.js
@@ -0,0 +1,45 @@
+import makeStyles from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+    backgroundColor: theme.palette.background.dark,
+  },
+  appBar: {
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: 'none',
+  },
+  logo: {
+    height: 25,
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(6),
+  },
+  icons: {
+    paddingRight: theme.spacing(4),
+  },
+  grow: {
+    flexGrow: '1',
+  },
+  listItemText: {
+    fontSize: 14,
+  },
+  listItem: {
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  subheader: {
+    textTransform: 'uppercase',
+  },
+}));
+
+export default useStyles;
